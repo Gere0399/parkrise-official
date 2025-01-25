f9328e15c238cfb7f55ba225ae6404c7699b2199
romi <abdulrehman012859@gmail.com>
2025-01-25 11:31:51 +0500

    2

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -18,60 +18,80 @@ export function Nav() {
   };
 
   return (
-    <div className="main">
-      <nav className="main_content">
-        <div className="bg-white">
-          <div className="main">
-            <div className="flex justify-between items-center py-6 px-6 lg:px-12 main_content">
-              <img
-                src={
-                  "/lovable-uploads/3232ef68-4d90-47b1-af6c-cbce7ac2c0e5.png"
-                }
-                alt="Parkrise Logo"
-                className="md:h-8 h-4 w-auto"
-              />
-              <div className="flex justify-center items-center gap-8">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Explicabo temporibus reiciendis praesentium odio voluptate saepe
-                corporis modi. Sequi nulla accusamus atque tempora eius ad
-                blanditiis mollitia, fugiat error quibusdam quos.
-              </div>
-              <div className="flex justify-start items-center gap-8">
-                <img
-                  src="/icons/left-arrow.svg"
-                  alt="back arrow"
-                  onClick={handleOpenSidebar}
-                />
-              </div>
-            </div>
-          </div>
+    <nav>
+      <div className="max-w-[1440px] mx-auto ">
+        <div className="flex justify-between items-center py-6   mx-4 lg:mx-[20px] xl:mx-[60px] ">
+          <img
+            src={"/lovable-uploads/b64ac30e-4d72-4f40-94dc-da41b281490e.png"}
+            alt="Parkrise Logo"
+            className="w-[131px] h-[15px]"
+          />
+          <ul className="justify-start items-center gap-5 hidden md:flex lg:mr-[180px]">
+            <li className="font-montserrat text-[14px] font-medium leading-[30px] text-center [text-underline-position:from-font] [text-decoration-skip-ink:none] hover:text-[#00B2B2] cursor-pointer">
+              Home
+            </li>
+            <li className="font-montserrat text-[14px] font-medium leading-[30px] text-center [text-underline-position:from-font] [text-decoration-skip-ink:none] hover:text-[#00B2B2] cursor-pointer">
+              Destinations
+            </li>
+            <li className="font-montserrat text-[14px] font-medium leading-[30px] text-center [text-underline-position:from-font] [text-decoration-skip-ink:none] hover:text-[#00B2B2] cursor-pointer">
+              Our brand
+            </li>
+            <li className="font-montserrat text-[14px] font-medium leading-[30px] text-center [text-underline-position:from-font] [text-decoration-skip-ink:none] hover:text-[#00B2B2] cursor-pointer">
+              Franchise
+            </li>
+            <li className="font-montserrat text-[14px] font-medium leading-[30px] text-center [text-underline-position:from-font] [text-decoration-skip-ink:none] hover:text-[#00B2B2] cursor-pointer">
+              For businesses
+            </li>
+          </ul>
+          {/* Todo- menu icon */}
+          <img
+            src="/icons/left-arrow.svg"
+            alt="back arrow"
+            onClick={handleOpenSidebar}
+            className="block md:hidden"
+          />
         </div>
-        <div
-          ref={sidebarRef}
-          className={` bg-white translate-x-[120%] shadow-primary  w-full h-full max-h-full  fixed z-[50] shadow-grey transition-transform duration-[0.5s] ease-[ease-in-out] overflow-y-auto left-0 top-0 p-4`}
-        >
-          <div>
-            <div className="flex justify-end items-center w-full">
-              <img src="/icons/left-arrow.svg" alt="back arrow" />
-            </div>
-            <div className="flex justify-center items-center">
-              <img
-                src={
-                  "/lovable-uploads/3232ef68-4d90-47b1-af6c-cbce7ac2c0e5.png"
-                }
-                alt="Parkrise Logo"
-                className="md:h-8 h-4 w-auto"
-              />
-            </div>
-            <div className="flex flex-col justify-center items-center gap-8 w-full mt-8">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti
-              hic, deleniti quo velit, quae rem, optio adipisci sunt minima enim
-              reprehenderit? Fugit obcaecati dolor esse corrupti illo voluptates
-              voluptate quod.
-            </div>
+      </div>
+
+      <div
+        ref={sidebarRef}
+        className={` bg-white  md:hidden translate-x-[120%] shadow-primary  w-full h-full max-h-full  fixed z-[50] shadow-grey transition-transform duration-[0.5s] ease-[ease-in-out] overflow-y-auto left-0 top-0 p-4`}
+      >
+        <div>
+          <div className="flex justify-end items-center w-full">
+            <img
+              src="/icons/left-arrow.svg"
+              alt="back arrow"
+              onClick={handleCloseSidebar}
+            />
           </div>
+          <div className="flex justify-center items-center">
+            {/* Todo- cross icon */}
+            <img
+              src={"/lovable-uploads/b64ac30e-4d72-4f40-94dc-da41b281490e.png"}
+              alt="Parkrise Logo"
+              className="w-[131px] h-[15px]"
+            />
+          </div>
+          <ul className="flex flex-col justify-start items-center gap-5   mt-10">
+            <li className="font-montserrat text-[14px] font-medium leading-[30px] text-center [text-underline-position:from-font] [text-decoration-skip-ink:none] hover:text-[#00B2B2] cursor-pointer">
+              Home
+            </li>
+            <li className="font-montserrat text-[14px] font-medium leading-[30px] text-center [text-underline-position:from-font] [text-decoration-skip-ink:none] hover:text-[#00B2B2] cursor-pointer">
+              Destinations
+            </li>
+            <li className="font-montserrat text-[14px] font-medium leading-[30px] text-center [text-underline-position:from-font] [text-decoration-skip-ink:none] hover:text-[#00B2B2] cursor-pointer">
+              Our brand
+            </li>
+            <li className="font-montserrat text-[14px] font-medium leading-[30px] text-center [text-underline-position:from-font] [text-decoration-skip-ink:none] hover:text-[#00B2B2] cursor-pointer">
+              Franchise
+            </li>
+            <li className="font-montserrat text-[14px] font-medium leading-[30px] text-center [text-underline-position:from-font] [text-decoration-skip-ink:none] hover:text-[#00B2B2] cursor-pointer">
+              For businesses
+            </li>
+          </ul>
         </div>
-      </nav>
-    </div>
+      </div>
+    </nav>
   );
 }
